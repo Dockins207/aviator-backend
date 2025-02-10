7805f1ee63b2f232c73b26b2f46aeca4d80c9fbb
Dockins <otienodockins48@gmail.com>
2025-02-10 20:55:46 +0300

    separate frontend

--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-900">
-      <h1 className="text-6xl font-bold text-white">Aviator</h1>
-    </div>
-  );
-}
